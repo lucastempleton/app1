576a47aa50c93ac1f7d3cca5bdb3a86ab3dc4abf
cR26i7HcmuXRBA <pj83o0@protonmail.com>
2022-01-04 01:21:02 -0600

    nice :)

--- open_note/src/App.js
+++ open_note/src/App.js
@@ -3,26 +3,19 @@ import './App.css';
 import { useEffect, useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import NoAuthApp from './components/NoAuthApp';
-import Homepage from './components/Homepage'
+import Homepage from './components/Homepage';
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  function login(){
-    setIsLoggedIn(true)
-  }
-  function logout(){
-    console.log('Before:'+isLoggedIn)
-    setIsLoggedIn(false)
-    console.log('button clicked')
-    console.log('After:', isLoggedIn)
-  }
-  return (
   
-      <div>
-      {isLoggedIn ?  <Homepage logout={logout}/> : <NoAuthApp login={login}/> }
-      </div>
-   
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div className="App high">
+      App.js
+    </div>
   );
 }
 
